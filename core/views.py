1aceff7af651f6dcb87f0514cb5b8f452297ed8e
Junaid Saleem <junaidsaleem986@gmail.com>
2024-07-23 13:38:28 +0500

    static files and basic templates

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request,'base.html')
+
+def addExpense(request):
+    return render(request,'core/add-expense.html')
