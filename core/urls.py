df8c47745d2357bbb499437052768eaac2cc9e07
Junaid Saleem <junaidsaleem986@gmail.com>
2024-08-06 14:17:31 +0500

    Add Expense

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('',views.index ,name='home'),
-    path('add-expense/',views.addExpense ,name='add-expense'),
+    path('add-expense/',views.addExpense.as_view() ,name='add-expense'),
+    path('expenses/',views.expenses ,name='expenses'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.views import View
+from .models import Category, Expense
+from django.utils.decorators import method_decorator
+from django.contrib import messages
 
 # Create your views here.
 
@@ -7,6 +11,35 @@ from django.contrib.auth.decorators import login_required
 def index(request):
     return render(request,'base.html')
 
+@method_decorator(login_required(login_url='login'), name='dispatch')
+class addExpense(View):
+    def get(self,request):
+        categories = Category.objects.all()
+
+        context = {
+            'categories':categories,
+            'values':request.POST
+        }
+        return render(request,'core/add-expense.html',context)
+
+    def post(self,request):
+        amount = request.POST['amount']
+        description = request.POST['description']
+        date = request.POST['date']
+        category_name = request.POST['category']
+        category = Category.objects.get(name = category_name)
+        user = request.user
+
+        if not amount or not description:
+            messages.error(request,'Fill all Fields')
+            return redirect('add-expense')
+        else:
+            Expense.objects.create(amount = amount,description = description,date = date,category = category, user = user)
+            messages.success(request,'Expense Added Successfully')
+            return redirect('home')
+        
+
+
 @login_required(login_url='login')
-def addExpense(request):
-    return render(request,'core/add-expense.html')
+def expenses(request):
+    return render(request,'core/expenses.html')
