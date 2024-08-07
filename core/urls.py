77dee452405abd917f0f0913848bf5034a970ad5
Junaid Saleem <junaidsaleem986@gmail.com>
2024-08-07 14:06:04 +0500

    edit and delete expense

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('',views.index ,name='home'),
     path('add-expense/',views.addExpense.as_view() ,name='add-expense'),
+    path('edit-expense/<int:id>',views.editExpense ,name='edit-expense'),
+    path('delete-expense/<int:id>',views.deleteExpense ,name='delete-expense'),
     path('expenses/',views.expenses ,name='expenses'),
 ]
--- core/views.py
+++ core/views.py
@@ -4,6 +4,7 @@ from django.views import View
 from .models import Category, Expense
 from django.utils.decorators import method_decorator
 from django.contrib import messages
+from preferences.models import UserPreference
 
 # Create your views here.
 
@@ -36,10 +37,74 @@ class addExpense(View):
         else:
             Expense.objects.create(amount = amount,description = description,date = date,category = category, user = user)
             messages.success(request,'Expense Added Successfully')
-            return redirect('home')
+            return redirect('expenses')
+
+
+@login_required(login_url='login')
+def editExpense(request,id):
+    try:
+        expense = Expense.objects.get(pk=id)
+    except:
+        messages.error(request,'Expense does not Exist!')
+        return redirect('expenses')
+    categories = Category.objects.all()
+    user = request.user
+    if expense.user == user:
+        context = {
+            'expense':expense,
+            'categories':categories
+        }
+        if request.method=='GET':
+            return render(request,'core/edit-expense.html',context)
+        else:
+            amount = request.POST['amount']
+            description = request.POST['description']
+            date = request.POST['date']
+            category_name = request.POST['category']
+            category = Category.objects.get(name = category_name)
+
+            if not amount or not description:
+                messages.error(request,'Fill all Fields')
+                return render(request,'core/edit-expense.html',context)
+            else:
+                expense.amount = amount
+                expense.description = description
+                expense.date = date
+                expense.category = category
+                expense.save()
+                messages.success(request,'Expense Updated Successfully')
+                return redirect('expenses')
+    else:
+        messages.error(request,'Access Denied')
+        return redirect('expenses')
+
         
+@login_required(login_url='login')
+def deleteExpense(request,id):
+    try:
+        expense = Expense.objects.get(pk=id)
+    except:
+        messages.error(request,'Expense does not Exist!')
+        return redirect('expenses')
+    if expense.user == request.user:
+        expense.delete()
+        return redirect('expenses')
+    else:
+        messages.error(request,'Access Denied')
+        return redirect('expenses')
+
+    
 
 
 @login_required(login_url='login')
 def expenses(request):
-    return render(request,'core/expenses.html')
+    try:
+        currency = UserPreference.objects.get(user = request.user).currency
+    except:
+        currency = ""
+    expenses = Expense.objects.filter(user = request.user)
+    context = {
+        "currency": currency,
+        "expenses": expenses
+        }
+    return render(request,'core/expenses.html',context)
