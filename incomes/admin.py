a617884c05c59f4284b2da748facefca31a52fc1
Junaid Saleem <junaidsaleem986@gmail.com>
2024-08-10 12:57:15 +0500

    admin customization

--- incomes/admin.py
+++ incomes/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Income)
+class IncomeAdmin(admin.ModelAdmin):
+    list_display = ('amount', 'description', 'user', 'source', 'date',)
+    search_fields = ('description', 'source__name', 'date','amount')
+
+    list_per_page = 10
+
+admin.site.register(Income, IncomeAdmin)
 admin.site.register(Source)
